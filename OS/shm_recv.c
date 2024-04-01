22fd90795275865f3d3b24fb9aa28ae715ad43f4
Mahadev Panchal <upvord@gmail.com>
2024-04-01 15:27:53 +0530

    IPC SHMs

---
+++ OS/shm_recv.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/shm.h>
+#include <unistd.h>
+int main() {
+  int i;
+  void *shared_memory;
+  char buff[100];
+  int shmid;
+  shmid = shmget((key_t)2222, 1024, 0666);
+  if (shmid == -1) {
+    perror("shmget");
+    return -1;
+  }
+  // creates shared memory segment with key 2345, having size 1024 bytes.
+  // IPC_CREAT is used to create the shared segment if it does not exist. 0666
+  // are the permissions on the shared segment
+  printf("Key of shared memory is %d\n", shmid);
+  shared_memory = shmat(shmid, NULL, 0);
+  if (shared_memory == NULL) {
+    perror("shmat");
+    return -1;
+  }
+  // process attached to shared memory segment
+  printf("Process attached at %p\n", shared_memory);
+  // this prints the address where the segment is attached with this process
+  strcpy(buff, shared_memory); // data written to shared memory
+  printf("You read from SHM: %s\n", buff);
+}
